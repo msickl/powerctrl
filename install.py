d67a7ad2603c5617372f7a178ae88ed9b64de041
Sickl Martin <office@sickl.at>
2023-10-17 08:49:51 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,7 +1,14 @@
-ssh-keygen -t rsa -b 4096 -C "office@sickl.at" 
-ssh-copy-id admin@10.0.0.250
+import subprocess
+from lib import config
 
-ln -s /opt/powerctrl/etc/powerctrl.service /etc/systemd/system/
-systemctrl enable powerctrl.service
-systemctrl daemon-reload
-systemctrl start powerctrl.service
+# Generate an SSH key pair
+subprocess.run('ssh-keygen -t rsa -b 4096 -C "office@sickl.at"', shell=True)
+
+# Copy the SSH public key to the remote server
+subprocess.run('ssh-copy-id admin@10.0.0.250', shell=True)
+
+# Create a symbolic link and configure the systemd service
+subprocess.run('ln -s /opt/powerctrl/etc/powerctrl.service /etc/systemd/system/', shell=True)
+subprocess.run('systemctl enable powerctrl.service', shell=True)
+subprocess.run('systemctl daemon-reload', shell=True)
+subprocess.run('systemctl start powerctrl.service', shell=True)
