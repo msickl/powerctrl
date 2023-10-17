02958f8158176c6edd66697cfb878b18a3356208
Sickl Martin <office@sickl.at>
2023-10-17 08:44:32 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,2 +1,7 @@
 ssh-keygen -t rsa -b 4096 -C "office@sickl.at" 
 ssh-copy-id admin@10.0.0.250
+
+ln -s /opt/powerctrl/etc/powerctrl.service /etc/systemd/system/
+systemctrl enable powerctrl.service
+systemctrl daemon-reload
+systemctrl start powerctrl.service
