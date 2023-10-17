4c6be329bb5b2c9b9997d38eb10a97f7feb79e02
Sickl Martin <office@sickl.at>
2023-10-17 08:53:30 +0200

    Update install.py

--- install.py
+++ install.py
@@ -2,10 +2,10 @@ import subprocess
 from lib import config as cfg
 
 # Generate an SSH key pair
-subprocess.run('ssh-keygen -t rsa -b 4096 -C "office@sickl.at"', shell=True)
+subprocess.run('ssh-keygen -t rsa -b 4096', shell=True)
 
 # Copy the SSH public key to the remote server
-cmd = f"ssh-copy-id admin@{cfg.switch('Address')}"
+cmd = f"ssh-copy-id {cfg.switch('Username')}@{cfg.switch('Address')}"
 subprocess.run(cmd, shell=True)
 
 # Create a symbolic link and configure the systemd service
