f2987d3e01c33318c1a77c9b3bc8d7b0099c4e40
Sickl Martin <office@sickl.at>
2023-10-17 10:35:38 +0200

    Update install.py

--- install.py
+++ install.py
@@ -4,13 +4,16 @@ import subprocess
 from lib import config as cfg
 
 # Generate an SSH key pair
+print("Generate an SSH key pair")
 subprocess.run('ssh-keygen -t rsa -b 4096', shell=True)
 
 # Copy the SSH public key to the remote server
+print("Copy the SSH public key to the remote server")
 cmd = f"ssh-copy-id {cfg.switch('Username')}@{cfg.switch('Address')}"
 subprocess.run(cmd, shell=True)
 
 # Create a symbolic link and configure the systemd service
+print("Create a symbolic link and configure the systemd service")
 subprocess.run('ln -s /opt/powerctrl/etc/powerctrl.service /etc/systemd/system/', shell=True)
 subprocess.run('systemctl enable powerctrl.service', shell=True)
 subprocess.run('systemctl daemon-reload', shell=True)
