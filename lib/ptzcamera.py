abf3995b3147905b72097795c0ad965794a6ad87
SICKL Martin <office@sickl.at>
2025-03-10 17:32:40 +0100

    Update ptzcamera.py

--- lib/ptzcamera.py
+++ lib/ptzcamera.py
@@ -2,11 +2,14 @@
 
 import requests
 
-def setposition(server, pos):
+class NVRDisplayController:
+    def __init__(self, server):
+        self.server = server
 
-    url = f"http://{server}/cgi-bin/ptzctrl.cgi?ptzcmd&poscall&{pos}"
-    res = requests.get(url, timeout=5)
-    if res.status_code == 200:
-        print(f"Position {pos} has been set.")
-    else:
-        print(f"There was a problem to set the position {pos}")
+    def setposition(self, pos):
+        url = f"http://{self.server}/cgi-bin/ptzctrl.cgi?ptzcmd&poscall&{pos}"
+        res = requests.get(url, timeout=5)
+        if res.status_code == 200:
+            print(f"Position {pos} has been set.")
+        else:
+            print(f"There was a problem to set the position {pos}")
