6eac97e5a28d6481c1570b7b9e7ef1a9dee62008
SICKL Martin <office@sickl.at>
2025-03-10 17:26:25 +0100

    Update nvrdisplay.py

--- lib/nvrdisplay.py
+++ lib/nvrdisplay.py
@@ -4,32 +4,41 @@ import json
 import subprocess
 from . import wol
 
-def shutdown(server, port):
-    url = f"http://{server}:{port}"
-    headers = {
-        "Content-Type": "application/json"
-    }
+class NVRDisplayController:
+    def __init__(self, server, username, password):
+        self.server = server
+        pjauth = f"{username}:{password}"
+        pjauthbytes = base64.b64encode(pjauth.encode("utf-8"))
+        self.headers = {
+            "Authorization": f"Basic {pjauthbytes.decode('utf-8')}"
+        }
 
-    data = {
-        "shutdown": "true"
-    }
-    body = json.dumps(data)
-    curl_command = [
-        "curl",
-        "-s",
-        "-X", "POST",
-        "-H", f"Content-Type: {headers['Content-Type']}",
-        "--data", body,
-        url,
-        "--insecure"
-    ]
-    try:
-        response = subprocess.check_output(curl_command, stderr=subprocess.STDOUT, text=True, timeout=2)
-        print(response)
-    except subprocess.TimeoutExpired:
-        print("NVRIntServer is already offline.")
-    except subprocess.CalledProcessError as e:
-        print(f"Request failed with error: {e.returncode}\n{e.output}")
-
-def start(server, port):
-    wol.send(server, port)
+    def shutdown(server, port):
+        url = f"http://{server}:{port}"
+        headers = {
+            "Content-Type": "application/json"
+        }
+    
+        data = {
+            "shutdown": "true"
+        }
+        body = json.dumps(data)
+        curl_command = [
+            "curl",
+            "-s",
+            "-X", "POST",
+            "-H", f"Content-Type: {headers['Content-Type']}",
+            "--data", body,
+            url,
+            "--insecure"
+        ]
+        try:
+            response = subprocess.check_output(curl_command, stderr=subprocess.STDOUT, text=True, timeout=2)
+            print(response)
+        except subprocess.TimeoutExpired:
+            print("NVRIntServer is already offline.")
+        except subprocess.CalledProcessError as e:
+            print(f"Request failed with error: {e.returncode}\n{e.output}")
+    
+    def start(server, port):
+        wol.send(server, port)
